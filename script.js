6f9354d1d2d872daa35dd178f7d9da1932e9da31
Joana <joanamdsantos1@gmail.com>
2022-07-20 19:25:02 -0300

    quinto exercício concluído

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const num = -2;  
+const ang1 = 40; 
+const ang2 = 80;
+const ang3 = 60; 
+const validAng = 180; 
 
-if (num < 0) {
-    console.log("negative");
+let somaAng = (ang1 + ang2 + ang3);
+
+if (somaAng == validAng) {
+    console.log("true");
 } else {
-    console.log("positive");
+    console.log("false");
 }
