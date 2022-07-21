923776133835e0369851b95903d527eea84b0d39
Joana <joanamdsantos1@gmail.com>
2022-07-21 19:20:47 -0300

    quarto exercício finalizado

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-soma = 0;
-
-for (let index = 0; index < numbers.length; index += 1) { 
-    soma += numbers[index];
-    media = soma / numbers.length;
+for (let index = 0; index < numbers.length; index += 1) {
+    lastIndex = numbers.length[index]
 }
-
-console.log(media);
+if (lastIndex > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log('valor menor ou igual a 20');
+}
