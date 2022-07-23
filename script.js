8c0af0da7831714586964612187e1d6d74d27500
Joana <joanamdsantos1@gmail.com>
2022-07-23 18:20:21 -0300

    código para guardar os números finalizado

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // 1- O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
 let numbers = [];
+let fatorial = 0;
 
 for (let index = 1; index < 11; index += 1) {
     numbers.push(index);
-    console.log(numbers);
+} 
+console.log(numbers);
 
-}
 
