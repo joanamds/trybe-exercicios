af3c1d2acff6c5857fc86281214eb505968ab73f
Joana <joanamdsantos1@gmail.com>
2022-07-25 11:32:31 -0300

    primeiro exercício solucionado

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // 1- O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
-let numbers = [];
-let fatorial = 0;
 
-for (let index = 1; index < 11; index += 1) {
-    numbers.push(index);
+// Entrada 
+let fatorial = 1; 
+
+// Processamento
+for (let index = 10; index > 0; index -= 1) {
+        fatorial *= index;
 } 
-console.log(numbers);
+
+//Saída
+console.log(fatorial);
 
 
